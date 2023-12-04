f064ff13a730ac82151f46286888ec96d20b087e
lafouine022 <119431960+lafouine022@users.noreply.github.com>
2023-12-04 22:32:16 +0100

    Add files via upload

---
+++ seedbox-info/lib/ftps_xml.php
@@ -0,0 +1,59 @@
+<?php
+$ServeurName = $_SERVER['SERVER_NAME']; //nom du serveur
+$ServeurIp = $_SERVER['HTTP_HOST']; //Ip du serveur
+$UserName = $_SERVER['PHP_AUTH_USER']; //username
+$Pass = $_SERVER['PHP_AUTH_PW'];  //mot de passe
+//Debut xml
+header('Content-Disposition: attachment; filename="Filezilla-FTPS.xml"');
+header('Content-type: "text/xml"; charset="utf8"');	
+$xml = '<?xml version="1.0" encoding="UTF-8"?>'; 
+   $xml .= '<FileZilla3><Servers><Server>';
+   $xml .= '<Host>';
+   $xml .= ''.$ServeurName.'';
+   $xml .= '</Host>';
+   $xml .= '<Port>';
+   $xml .= '3339';
+   $xml .= '</Port>';
+    $xml .= '<Protocol>';
+   $xml .= '0';
+   $xml .= '</Protocol>';
+   $xml .= '<Type>';
+   $xml .= '1';
+   $xml .= '</Type>';
+   $xml .= '<User>';
+   $xml .= ''.$UserName.'';
+   $xml .= '</User>';
+   $xml .= '<Pass>';
+   $xml .= ''.$Pass.'';
+   $xml .= '</Pass>';
+   $xml .= '<Logontype>';
+   $xml .= '1';
+   $xml .= '</Logontype>';
+   $xml .= '<TimezoneOffset>';
+   $xml .= '0';
+   $xml .= '</TimezoneOffset>';
+   $xml .= '<PasvMode>';
+   $xml .= 'MODE_DEFAULT';
+   $xml .= '</PasvMode>';
+   $xml .= '<MaximumMultipleConnections>';
+   $xml .= '0';
+   $xml .= '</MaximumMultipleConnections>';
+   $xml .= '<EncodingType>';
+   $xml .= 'UTF-8';
+   $xml .= '</EncodingType>';
+   $xml .= '<BypassProxy>';
+   $xml .= '0';
+   $xml .= '</BypassProxy>';
+   $xml .= '<Name>';
+   $xml .= 'Seedbox-'.$UserName.'-FTPS';
+   $xml .= '</Name>';
+   $xml .= '<SyncBrowsing>';
+   $xml .= '0';
+   $xml .= '</SyncBrowsing>'; 
+   //$xml .= '<DirectoryComparison>';
+   //$xml .= '0';
+  // $xml .= '</DirectoryComparison>';  
+   $xml .= '</Server></Servers></FileZilla3>'; 
+   echo $xml; 
+	//exit();
+?>
---
+++ seedbox-info/lib/transdroids_xml.php
@@ -0,0 +1,47 @@
+<?php
+$ServeurName = $_SERVER['SERVER_NAME']; //nom du serveur
+$ServeurIp = $_SERVER['HTTP_HOST']; //Ip du serveur
+$UserName = $_SERVER['PHP_AUTH_USER']; //username
+$Pass = $_SERVER['PHP_AUTH_PW'];  //mot de passe
+
+//Debut json
+header('Content-Disposition: attachment; filename="settings.json"');
+header('Content-type: application/json');	
+
+ $json = json_encode( array( 'ui_swipe_labels' => false,
+    'alarm_vibrate' => false,
+    'alarm_enabled' => false,
+	'alarm_enabled' => false,
+    'alarm_check_rss_feeds' => false,
+	'websites' => array(),
+	'ui_refresh_interval' => "60",
+	'ui_hide_refresh' => false,
+    'search_sort_by' => "sort_seeders",
+	'alarm_play_sound' => false,
+	'ui_enable_ads' => true,
+	'ui_only_show_transferring' => false,
+    'search_num_results' => "sort_seeders",
+	'alarm_play_sound' => "25",
+	'servers' => array( array(
+	            'port' => "443",
+                'host' => "https://".$ServeurName."",
+	            'ssl' => true,
+                'type' => "daemon_rtorrent",
+	            'password' => $Pass,
+	            'os_type' => "type_linux",
+	            'folder' => "/".strtoupper($UserName)."",
+                'username' => $UserName,
+	            'use_auth' => true,
+	            'name' => "Seedbox-".$UserName."",
+	            'base_ftp_url' => "ftp://".$UserName."@".$ServeurName."/torrents/",
+                'download_alarm' => true,
+	            'new_torrent_alarm' => true,
+				'ssl_accept_all' => false
+	)),
+	'alarm_interval' => "600000",
+    'rssfeeds' => array(),
+	'ui_ask_before_remove' => true )); 
+         
+echo( $json);
+
+?>
